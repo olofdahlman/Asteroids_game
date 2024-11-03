17b0ec5c1a40b91b706f870d14ae3591cdb3caab
olofdahlman <olofdahlman@live.se>
2024-11-03 22:22:34 +0100

    3: Added CircleShape and Player classes for handling shapes and the players sprite and added rendering of this sprite to main.

--- main.py
+++ main.py
@@ -4,20 +4,26 @@
 #Use python3 main.py rather than ./main.py to start the program, close with ctrl-Cs
 
 import pygame
+from player import Player
 from constants import *
 
 def main():
         pygame.init()
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #Defines the window of the game, aka the graphical user interface (GUI)
+        fps = pygame.time.Clock()
+        dt = 0
         print("Starting asteroids!",
               f"Screen width: {SCREEN_WIDTH}",
               f"Screen height: {SCREEN_HEIGHT}")
+        player_sprite = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
         while True:    #Infinite while loop - terminate with ctrl - C in terminal
               for event in pygame.event.get():
                   if event.type == pygame.QUIT:
                         return
               screen.fill('black')
+              player_sprite.draw(screen)
               pygame.display.flip()
+              dt = (fps.tick(60)/1000)
 
 
 
